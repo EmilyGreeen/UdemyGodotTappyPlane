45dff7c72c0f680026b8e927246468f94ee5ffb9
Mgreeen <lakc42@gmail.com>
2024-11-14 19:09:59 +0100

    add dying if plane hits ceiling or floor

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const GRAVITY: float = 1000.0
 const POWER: float = -200.0
+@onready var sprite: AnimatedSprite2D = $Sprite
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,9 +18,17 @@ func _physics_process(delta: float) -> void:
 	fly()
 	
 	move_and_slide()
+	
+	if is_on_floor() or is_on_ceiling():
+		die()
 
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		print("jump!")
 		velocity.y = POWER
+
+
+func die() -> void:
+	set_physics_process(false)
+	sprite.stop()
